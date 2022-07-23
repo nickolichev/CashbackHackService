4368384f7c7e025eee0f989973b01832243b351b
nickolichev@gmail.com <nickolichev@gmail.com>
2022-07-23 22:52:30 +0300

    Junit4

---
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.unit;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+  CashbackHackService service = new CashbackHackService();
+
+  @org.junit.Test
+  public void remainTestFirst() {
+
+    int expected = 200;
+    int amount = 800;
+    int result = service.remain(amount);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void remainTestSecond() {
+
+    int expected = 1;
+    int amount = 999;
+    int result = service.remain(amount);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void remainTestThird() {
+
+    int expected = 999;
+    int amount = 1001;
+    int result = service.remain(amount);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void remainFalse() {
+
+    int expected = 0;
+    int amount = 1000;
+    int result = service.remain(amount);
+
+    assertEquals(expected, result);
+  }
+}
