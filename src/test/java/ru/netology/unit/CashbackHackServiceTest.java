b2aaab477bedf21e1486fcc3f3d1ce61ebe7e4e1
nickolichev@gmail.com <nickolichev@gmail.com>
2022-07-23 23:48:48 +0300

    junit4-platform

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.unit;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -9,41 +10,66 @@ public class CashbackHackServiceTest {
 
   @org.junit.Test
   public void remainTestFirst() {
-
     int expected = 200;
     int amount = 800;
     int result = service.remain(amount);
-
     assertEquals(expected, result);
   }
 
   @org.junit.Test
   public void remainTestSecond() {
-
     int expected = 1;
     int amount = 999;
     int result = service.remain(amount);
-
     assertEquals(expected, result);
   }
 
   @org.junit.Test
   public void remainTestThird() {
-
     int expected = 999;
     int amount = 1001;
     int result = service.remain(amount);
-
     assertEquals(expected, result);
   }
 
   @org.junit.Test
   public void remainFalse() {
-
     int expected = 0;
     int amount = 1000;
     int result = service.remain(amount);
 
     assertEquals(expected, result);
+  }
+
+  @Test
+  public void remainTestFirstAPI() {
+    int expected = 200;
+    int amount = 800;
+    int result = service.remain(amount);
+    Assertions.assertEquals(expected, result);
+  }
+
+  @Test
+  public void remainTestSecondAPI() {
+    int expected = 1;
+    int amount = 999;
+    int result = service.remain(amount);
+    Assertions.assertEquals(expected, result);
+  }
+
+  @Test
+  public void remainTestThirdAPI() {
+    int expected = 999;
+    int amount = 1001;
+    int result = service.remain(amount);
+    Assertions.assertEquals(expected, result);
+  }
+
+  @Test
+  public void remainFalseAPI() {
+    int expected = 0;
+    int amount = 1000;
+    int result = service.remain(amount);
+    Assertions.assertEquals(expected, result);
   }
 }
