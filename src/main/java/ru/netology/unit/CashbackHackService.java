7f9710ee67f1bd61168ef3b2394abc3d05831281
nickolichev@gmail.com <nickolichev@gmail.com>
2022-08-01 20:10:01 +0300

    CI master

--- src/main/java/ru/netology/unit/CashbackHackService.java
+++ src/main/java/ru/netology/unit/CashbackHackService.java
@@ -2,7 +2,7 @@ package ru.netology.unit;
 
 public class CashbackHackService {
 
-    private final int boundary = 1000;
+    private final int boundary = 500;
 
     public int remain(int amount) {
       return boundary - amount % boundary;
